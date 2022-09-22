c9605be1353674b09267fb423d43c6615537a9f5
Lucas <lucas.costa4002@gmail.com>
2022-09-21 21:20:04 -0300

    feat: :sparkles: Add route test API

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,10 @@ import { createUserController } from "./useCases/CreateUser";
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.send({ msg: 'Application started' })
+})
+
 router.post("/users", (req, res) => {
   return createUserController.handle(req, res);
 });
